273784033d748d12484b047d9e40df932d00e2a3
Md Yusuf Nadaph <mdyusuf6421@gmail.com>
2023-12-25 21:11:36 +0545

    my tosolist

---
+++ src/component/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div className='header'>
+      <h1 className=''>TODO LIST</h1>
+      <hr />
+    </div>
+  )
+}
+
+export default Header;
